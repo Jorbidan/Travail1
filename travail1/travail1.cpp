566b49a1e6d98bec2a37d13df95e069d2e07455d
jordan <1641207@cegepba.qc.ca>
2020-02-12 13:46:41 -0500

    Melanger cartes et demande combien de carte à distribuer

--- travail1/travail1.cpp
+++ travail1/travail1.cpp
@@ -25,12 +25,41 @@ int main()
 	}
 }
 
-void Jouer() 
+void MelangerPaquet()
 {
-
+	int carte1;
+	int carte2;
+	Carte carteTempo;
+	for (int cpt = 0; cpt < 10000; cpt++)
+	{
+		carte1 = (rand() % 52) + 1;
+		carte2 = (rand() % 52) + 1;
+		carteTempo = leJeu.paquetCartes[carte1];
+		leJeu.paquetCartes[carte1] = leJeu.paquetCartes[carte2];
+		leJeu.paquetCartes[carte2] = carteTempo;
+	}
 }
 
-void initialiserNoms()
+void Distribution()
+{
+	int distribution;
+	cout << "combien de cartes voulez-vous distribué(max 26)?";
+	cin >> distribution;
+	if (distribution > maxCartesAJouer)
+	{
+		distribution = maxCartesAJouer;
+	}
+	/*distribuer les cartes*/
+}
+
+void Jouer() 
+{
+	MelangerPaquet();
+	Distribution();
+	
+}
+
+void InitialiserJoueurs()
 {
 	string nom;
 	cout << "\nQuel est le nom du joueur 1?\n";
@@ -39,11 +68,4 @@ void initialiserNoms()
 	cout << "\nQuel est le nom du joueur 2?\n";
 	cin >> nom;
 	leJeu.joueur2.SetNomJoueur(nom);
-}
-
-void InitialiserJoueurs()
-{
-	initialiserNoms();
-
-
 }
